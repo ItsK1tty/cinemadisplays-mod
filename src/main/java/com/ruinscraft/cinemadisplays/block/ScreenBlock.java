98520bfeb55f375a1e6d1c78545a7958bf1f642f
Dexton Anderson <contact@dextonanderson.com>
2021-06-07 15:38:59 -0500

    Add License headers

--- src/main/java/com/ruinscraft/cinemadisplays/block/ScreenBlock.java
+++ src/main/java/com/ruinscraft/cinemadisplays/block/ScreenBlock.java
@@ -1,3 +1,20 @@
+/*
+ * Cinema Displays Mod for Minecraft License
+ *
+ * Copyright (c) 2021 Ruinscraft, LLC
+ *
+ * This software is intellectual property of Ruinscraft, LLC and may not
+ * be modified, distributed, or used for commercial purposes without
+ * explicit written permission from the author.
+ *
+ * You may use this software for personal or testing purposes as long as
+ * you do not modify it, distribute it, or claim to be the original
+ * author.
+ *
+ * If you would like to license this software for commercial use, please
+ * email: andersond@ruinscraft.com
+ */
+
 package com.ruinscraft.cinemadisplays.block;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
--- src/main/java/com/ruinscraft/cinemadisplays/gui/widget/CheckBoxWhiteText.java
+++ src/main/java/com/ruinscraft/cinemadisplays/gui/widget/CheckBoxWhiteText.java
@@ -1,3 +1,20 @@
+/*
+ * Cinema Displays Mod for Minecraft License
+ *
+ * Copyright (c) 2021 Ruinscraft, LLC
+ *
+ * This software is intellectual property of Ruinscraft, LLC and may not
+ * be modified, distributed, or used for commercial purposes without
+ * explicit written permission from the author.
+ *
+ * You may use this software for personal or testing purposes as long as
+ * you do not modify it, distribute it, or claim to be the original
+ * author.
+ *
+ * If you would like to license this software for commercial use, please
+ * email: andersond@ruinscraft.com
+ */
+
 package com.ruinscraft.cinemadisplays.gui.widget;
 
 import com.mojang.blaze3d.platform.GlStateManager;
